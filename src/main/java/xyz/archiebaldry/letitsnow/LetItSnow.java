987fee9d8b47cf7a68db5d3ab73dfb682adf6cef
Archie Baldry <archiebaldry@protonmail.com>
2022-11-07 15:13:13 +0000

    Initial commit

---
+++ src/main/java/xyz/archiebaldry/letitsnow/LetItSnow.java
@@ -0,0 +1,13 @@
+package xyz.archiebaldry.letitsnow;
+
+import net.fabricmc.api.ModInitializer;
+
+public class LetItSnow implements ModInitializer {
+
+    public static final String MOD_ID = "letitsnow";
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
